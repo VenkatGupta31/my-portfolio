0f7eed3f452461829d99a42613ecf18dd37f5baf
Venkat Gupta <venkatgupta@Venkats-MacBook-Air.local>
2025-09-27 01:33:39 +0530

    latest next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? '/my-portfolio' : '',
-  assetPrefix: isProd ? '/my-portfolio/' : '',
 };
 
 module.exports = nextConfig;
